11b43efb9bef394018aba5408d9a7968f1c87a61
ttg-gh <ttg17@tutamail.com>
2025-06-13 09:04:41 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -106,5 +106,12 @@ sudo systemctl start ly.service
 # ----------------------------------------
 echo "🔗 Setting up ufw..."
 
+sudo ufw limit 22/tcp
+sudo ufw allow 80/tcp
+sudo ufw allow 443/tcp
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+sudo ufw enable
+
 echo "✅ Setup complete!"
 cd
